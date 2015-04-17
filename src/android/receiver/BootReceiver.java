f80dfccc6b5c1064c9cc3eeb67fc958c9ebf2a5d
Tomasz Pietrzak <tomasz.pietrzak@nopattern.net>
2015-04-17 10:09:14 +0200

    feat(plugin): Initialized plugin.

---
+++ src/android/receiver/BootReceiver.java
@@ -0,0 +1,14 @@
+package net.nopattern.cordova.beacon.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import net.nopattern.cordova.beacon.service.RangingService;
+
+public class BootReceiver extends BroadcastReceiver
+{
+  public void onReceive(Context arg0, Intent arg1)
+  {
+  }
+}
---
+++ src/android/service/RangingService.java
@@ -0,0 +1,6 @@
+package net.nopattern.cordova.beacon.service;
+
+import android.app.Service;
+
+public class RangingService extends Service {
+}
