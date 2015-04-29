f1c3f128958c0a823d7423d6061c744aca3f29f4
Tomasz Pietrzak <tomasz.pietrzak@nopattern.net>
2015-04-29 12:57:42 +0200

    feat(www): Registration and deregistration of events with the possibility of their call.

--- www/Beacon.js
+++ www/Beacon.js
@@ -1,5 +1,36 @@
 var exec = require('cordova/exec');
 
-exports.status = function(success, error) {
-    exec(success, error, "Beacon", "status", []);
+exports._listener = {};
+
+exports.fireEvent = function( event ) {
+  var args = Array.apply( null, arguments ).slice(1)
+    , listener = this._listener[event];
+
+  if( !this._listener[event] ) {
+    return;
+  }
+
+  for( var i = 0, j = this._listener[event].length; i < j; i++) {
+    var fn    = this._listener[event][i][0],
+        scope = this._listener[event][i][1];
+
+    fn.apply( scope, args );
+  }
+};
+
+exports.on = function( event, callback ) {
+  this._listener[event] = this._listener[event] || [];
+  this._listener[event].push([ callback, window ]);
+};
+
+exports.off = function( event, callback ) {
+  if( !this._listener[event] )
+    return;
+
+  for( var i = 0, j = this._listener[event].length; i < j; i++ ) {
+    if( this._listener[event][0] == callback ) {
+      this._listener[event].slice( i, 1 );
+      break;
+    }
+  }
 };
