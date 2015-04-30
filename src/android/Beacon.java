018221a53c6077c2df68922156afa2225c8c52e2
Tomasz Pietrzak <tomasz.pietrzak@nopattern.net>
2015-04-30 15:34:32 +0200

    feat(plugin): Event handling for android platform.

--- src/android/Beacon.java
+++ src/android/Beacon.java
@@ -1,5 +1,7 @@
 package net.nopattern.cordova.beacon;
 
+import android.content.Intent;
+
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
@@ -9,7 +11,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.content.Intent;
+import java.lang.Runnable;
+import java.lang.String;
 
 import net.nopattern.cordova.beacon.BeaconConstant;
 
@@ -18,29 +21,51 @@ import net.nopattern.cordova.beacon.BeaconConstant;
  */
 public class Beacon extends CordovaPlugin {
 
-  BeaconConstant bConstant;
+  // Reference to the web view for static access
+  private static CordovaWebView webView = null;
 
   @Override
   public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-    super.initialize(cordova, webView);
+    Beacon.webView = super.webView;
 
-    Intent intent = new Intent(bConstant.BOOT_MONITORING_INTENT);
+    Intent intent = new Intent( BeaconConstant.BOOT_MONITORING_INTENT);
     cordova.getActivity().sendBroadcast(intent);
+  }
+
+  public void onDeviceReady() {
+    intentHandler(cordova.getActivity().getIntent());
+  }
+
+  @Override
+  public void onNewIntent(Intent intent) {
+    intentHandler(intent);
+  }
+
+  private void intentHandler(Intent intent) {
+    String action = intent.getAction();
+
+    if( action.equals( BeaconConstant.NOTIFICATION_INTENT ) ) {
+      fireEvent("didReceiveLocalNotification");
+    }
+  }
+
+  private void fireEvent( String eventName ) {
+    final String js = "window.cordova.plugins.Beacon.fireEvent(" +
+      "\"" + eventName + "\")";
+
+    cordova.getActivity().runOnUiThread(new Runnable() {
+      public void run() {
+        webView.loadUrlIntoView("javascript:" + js, true);
+      }
+    });
   }
 
   @Override
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-    if (action.equals("status")) {
-      this.status(callbackContext);
+    if (action.equals("deviceready")) {
+      this.onDeviceReady();
       return true;
     }
     return false;
-  }
-
-  private void status(CallbackContext callbackContext) {
-    Intent notificationIntent = cordova.getActivity().getIntent();
-    String action = notificationIntent.getAction();
-
-    callbackContext.success("{\"notification\": " + action.equals(bConstant.NOTIFICATION_INTENT) + "}");
   }
 }
